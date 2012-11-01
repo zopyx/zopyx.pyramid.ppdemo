5a50ac63836faa3b4793737f65c217b6a07aa611
Andreas Jung <yet@gmx.de>
2012-11-01 18:19:10 +0100

    added

---
+++ README.txt
@@ -0,0 +1,13 @@
+Produce & Publish Showcase
+==========================
+
+Pyramid-based web-demo pacakage for demonstrating basic
+Produce & Publish features
+
+
+ZOPYX Limited
+c/o Andreas Jung
+Hundskapfklinge 33
+D-72074 Tuebingen, Germany
+info@zopyx.com
+www.zopyx.com
---
+++ ppdemo/views.py
@@ -0,0 +1,38 @@
+import os
+import mimetypes
+import tempfile
+from webob import Response
+from pyramid.view import view_config
+from zopyx.smartprintng.client.zip_client import Proxy2 as Proxy
+
+@view_config(route_name='home', renderer='templates/index.pt')
+def index(request):
+    return {'project':'pp-demo'}
+
+def build_html(ident, **kw):
+    filename = os.path.join(os.path.dirname(__file__), 'pdf-templates', '%s.pt' % ident)
+    html = open(filename).read() 
+    html = unicode(html, 'utf-8')
+    html = html % kw
+    return html.encode('utf-8')
+
+@view_config(name='generate-pdf')
+def generate_pdf(request):
+    text = request.params.get('text', '')
+    ident = request.params.get('ident', '')
+    html = build_html(ident, text=text)
+
+    proxy = Proxy('http://demo:demo@pp-server.zopyx.com')
+    dname = tempfile.mkdtemp()
+    fname = os.path.join(dname, 'index.html')
+    file(fname, 'wb').write(html)
+
+    result = proxy.convertZIP2(dname, converter_name='pdf-pdfreactor')
+    output_filename = result['output_filename']
+    ct, dummy = mimetypes.guess_type(output_filename)
+    basename, ext = os.path.splitext(output_filename)
+    converter = 'pdf-pdfreactor'
+    headers = [('content-disposition','attachment; filename=%s-%s%s' % (ident, converter,ext)), ('content-type', ct)]
+    return Response(body=file(output_filename, 'rb').read(),
+            content_type=ct,
+            headerlist=headers)
