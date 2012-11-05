4fe68a1b489adbca6e582a3f21e281b5728937a2
zopyx <yet@gmx.de>
2012-11-05 13:13:04 +0100

    updated

--- ppdemo/__init__.py
+++ ppdemo/__init__.py
@@ -1,9 +1,14 @@
 from pyramid.config import Configurator
 
+from kotti import conf_defaults as default_settings
+
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    settings2 = default_settings.copy()
+    settings2.update(settings) 
+    config = Configurator(settings=settings2)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.include('pyramid_mailer')
     config.add_route('home', '/')
