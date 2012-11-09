67d6adca95ab5105365685e6ee19f1e1f1c558f0
zopyx <yet@gmx.de>
2012-11-09 14:44:00 +0100

    refactored stuff

--- ppdemo/__init__.py
+++ ppdemo/__init__.py
@@ -1,14 +1,10 @@
 from pyramid.config import Configurator
 
-from kotti import conf_defaults as default_settings
-
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    settings2 = default_settings.copy()
-    settings2.update(settings) 
-    config = Configurator(settings=settings2)
+    config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.include('pyramid_mailer')
     config.add_route('home', '/')
--- ppdemo/views.py
+++ ppdemo/views.py
@@ -48,102 +48,109 @@ def send_mail(request):
     mailer.send(message)
     raise exc.HTTPFound('sent-mail-success')
 
-def build_html(ident, **kw):
-    filename = os.path.join(os.path.dirname(__file__), 'pdf-templates', '%s.pt' % ident)
-    html = open(filename).read() 
-    return html % kw
+@view_config(name='generate-pdf')
+class PDFRenderer(object):
 
-def fix_image_links(html, view_name):
-    """ We need to convert '../_images/xx.gif' links into  'images/xx.gif' etc. """
-    root = lxml.html.fromstring(html)
-    for img in root.xpath('.//img'):
-        src = img.attrib['src']
-        src = '/%s/%s' % (view_name, re.sub('(.*_images)', '_images', src))
-        img.attrib['src'] = src
-    return lxml.html.tostring(root, encoding=unicode)
+    def __init__(self, request):
+        self.request = request
 
+    def build_html(self, ident, **kw):
+        filename = os.path.join(os.path.dirname(__file__), 'pdf-templates', '%s.pt' % ident)
+        html = open(filename).read() 
+        return html % kw
 
-view_config(name='generate-pdf')
-def generate_pdf(request):
+    def __call__(self):
+        ident = str(self.request.params.get('ident'))
+        converter_name = str(self.request.params.get('converter_name', 'pdf-pdfreactor'))
 
-    ident = str(request.params.get('ident'))
-    converter_name = str(request.params.get('converter_name', 'pdf-pdfreactor'))
+        args = dict([(str(k), self.request.params.get(k, u'').encode('utf-8')) for k in self.request.params if k not in ('ident',)])
+        html = self.build_html(ident, **args)
 
-    args = dict([(str(k), request.params.get(k, u'').encode('utf-8')) for k in request.params if k not in ('ident',)])
-    html = build_html(ident, **args)
-
-    proxy = Proxy('http://demo:demo@pp-server.zopyx.com')
+        proxy = Proxy('http://demo:demo@pp-server.zopyx.com')
 #    proxy = Proxy('http://localhost:6543')
-    dname = tempfile.mkdtemp()
-    fname = os.path.join(dname, 'index.html')
-    file(fname, 'wb').write(html)
+        dname = tempfile.mkdtemp()
+        fname = os.path.join(dname, 'index.html')
+        file(fname, 'wb').write(html)
 
-    # copy stylesheets
-    templ_dir = os.path.join(os.path.dirname(__file__), 'pdf-templates')
-    for fn in os.listdir(templ_dir):
-        base, ext = os.path.splitext(fn)
-        if ext in ('.css', '.ttf'):
-            shutil.copy(os.path.join(templ_dir, fn), dname)
+        # copy stylesheets
+        templ_dir = os.path.join(os.path.dirname(__file__), 'pdf-templates')
+        for fn in os.listdir(templ_dir):
+            base, ext = os.path.splitext(fn)
+            if ext in ('.css', '.ttf'):
+                shutil.copy(os.path.join(templ_dir, fn), dname)
 
-    result = proxy.convertZIP2(dname, converter_name=converter_name)
-    output_filename = result['output_filename']
-    ct, dummy = mimetypes.guess_type(output_filename)
-    basename, ext = os.path.splitext(output_filename)
-    headers = [('content-disposition','attachment; filename=%s-%s%s' % (ident, converter_name, ext)), 
-               ('content-type', ct)]
-    return Response(body=file(output_filename, 'rb').read(),
-            content_type=ct,
-            headerlist=headers)
+        result = proxy.convertZIP2(dname, converter_name=converter_name)
+        output_filename = result['output_filename']
+        ct, dummy = mimetypes.guess_type(output_filename)
+        basename, ext = os.path.splitext(output_filename)
+        headers = [('content-disposition','attachment; filename=%s-%s%s' % (ident, converter_name, ext)), 
+                   ('content-type', ct)]
+        return Response(body=file(output_filename, 'rb').read(),
+                content_type=ct,
+                headerlist=headers)
 
 
-def render_sphinx(request, docroot, subdir='', view_name=''):
-    """ This traverser renders a Sphinx content page stored as
-        JSON below the 'documentation_json_' directory. The documentation
-        is generated from Sphinx using the JSON builder 
-        (or just "make json")
-    """
+class SphinxRenderer(object):
 
-    if not os.path.exists(docroot):
-        raise ValueError('%s does not exist' % docroot)
+    def __init__(self, request):
+        self.request = request
 
-    json_root = os.path.join(docroot, 'build', 'json', subdir)
-    image_root = os.path.join(docroot, 'build', 'json', '_images')
+    def fix_image_links(self, html, view_name):
+        """ We need to convert '../_images/xx.gif' links into  'images/xx.gif' etc. """
+        root = lxml.html.fromstring(html)
+        for img in root.xpath('.//img'):
+            src = img.attrib['src']
+            src = '/%s/%s' % (view_name, re.sub('(.*_images)', '_images', src))
+            img.attrib['src'] = src
+        return lxml.html.tostring(root, encoding=unicode)
 
-    dir_subpath = str(os.path.sep.join(request.subpath[:-1]))
-    last_item = str(request.subpath[-1])
-    base, ext = os.path.splitext(last_item)
-    json_filename = last_item + '.fjson'
-    docpath = os.path.join(json_root, dir_subpath, json_filename)
-    if os.path.exists(docpath):
-        d = json.loads(file(docpath, 'rb').read())
-        d['body'] = fix_image_links(d['body'], view_name)
-        return d
-    else:
-        if dir_subpath.startswith('_images'):
-            docpath = os.path.join(image_root, last_item)
+    def __call__(self):
+        """ This traverser renders a Sphinx content page stored as
+            JSON below the 'documentation_json_' directory. The documentation
+            is generated from Sphinx using the JSON builder 
+            (or just "make json")
+        """
+
+        docroot = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'parts', 'documentation-checkout', self.docroot))
+        if not os.path.exists(docroot):
+            raise ValueError('%s does not exist' % docroot)
+
+        json_root = os.path.join(docroot, 'build', 'json', self.subdir)
+        image_root = os.path.join(docroot, 'build', 'json', '_images')
+
+        dir_subpath = str(os.path.sep.join(self.request.subpath[:-1]))
+        last_item = str(self.request.subpath[-1])
+        base, ext = os.path.splitext(last_item)
+        json_filename = last_item + '.fjson'
+        docpath = os.path.join(json_root, dir_subpath, json_filename)
+        if os.path.exists(docpath):
+            d = json.loads(file(docpath, 'rb').read())
+            d['body'] = self.fix_image_links(d['body'], self.view_name)
+            d['home'] = dict(title=self.section_name, 
+                             link='/%s/index' % self.view_name)
+            return d
         else:
-            docpath = os.path.join(json_root, dir_subpath, last_item)
-        content_type = 'image/%s' % ext[1:]
-        content_type, encoding = mimetypes.guess_type(docpath)
-        headers = [('content-type', content_type)]
-        return Response(body=file(docpath, 'rb').read(),
-                        content_type=content_type,
-                        headerlist=headers)
+            if dir_subpath.startswith('_images'):
+                docpath = os.path.join(image_root, last_item)
+            else:
+                docpath = os.path.join(json_root, dir_subpath, last_item)
+            content_type = 'image/%s' % ext[1:]
+            content_type, encoding = mimetypes.guess_type(docpath)
+            headers = [('content-type', content_type)]
+            return Response(body=file(docpath, 'rb').read(),
+                            content_type=content_type,
+                            headerlist=headers)
+
+@view_config(name='references', renderer='templates/sphinx.pt')
+class ReferencesRenderer(SphinxRenderer):
+    view_name = 'references'
+    section_name = 'References'
+    docroot = 'www.produce-and-publish.com'
+    subdir = 'references'
 
 @view_config(name='documentation', renderer='templates/sphinx.pt')
-def docs(request, docroot='pp-docs'):
-    """ This traverser renders a Sphinx content page stored as
-        JSON below the 'documentation_json_' directory. The documentation
-        is generated from Sphinx using the JSON builder 
-        (or just "make json")
-    """
-    docroot = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'parts', 'documentation-checkout', docroot))
-    return render_sphinx(request, docroot, '', view_name='documentation')
-        
-@view_config(name='references', renderer='templates/sphinx.pt')
-def references(request, docroot='www.produce-and-publish.com'):
-    """ docroot = root of the SVN Sphinx checkout.
-        subdir = root of the subdirectory below <docroot>/build/json
-    """
-    docroot = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'parts', 'documentation-checkout', docroot))
-    return render_sphinx(request, docroot, 'references', view_name='references')
+class DocumentationRenderer(SphinxRenderer):
+    view_name = 'documentation'
+    section_name = 'Documentation'
+    docroot = 'pp-docs'
+    subdir = ''
